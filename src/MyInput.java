7fba8b16c40e01dbdb84c47c9dee616b543c002c
Yiwen <luyiwen0906@gmail.com>
2019-05-06 22:09:32 +0200

    add new file

---
+++ src/MyInput.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MyInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner inputs = new Scanner(System.in);
+		System.out.println("Your input is: " + inputs.nextLine());
+
+	}
+
+}
