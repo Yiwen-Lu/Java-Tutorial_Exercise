18a145fe81e95352bb3ec183bdec27dbfdd5379a
Yiwen <luyiwen0906@gmail.com>
2019-05-08 16:58:10 +0200

    add new file

---
+++ src/MyIf.java
@@ -0,0 +1,15 @@
+
+public class MyIf {
+
+	public static void main(String[] args) {
+		int x = 25;
+		if(x < 10) {
+			System.out.println("small");
+		} else if(x < 20) {
+			System.out.println("mdeium");
+		} else {
+			System.out.println("large");
+		}
+	}
+
+}
--- src/MyInput.java
+++ src/MyInput.java
@@ -3,10 +3,8 @@ import java.util.Scanner;
 public class MyInput {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner inputs = new Scanner(System.in);
 		System.out.println("Your input is: " + inputs.nextLine());
-
 	}
 
 }
